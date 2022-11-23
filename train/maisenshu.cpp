d5f7425028476393cef5c9a25f44632210ee9e18
DELLPC <2262944275@qq.com>
2022-11-23 21:16:13 +0800

    x22_11_23_21

--- train/maisenshu.cpp
+++ train/maisenshu.cpp
@@ -1,77 +1,268 @@
 //
 // Created by DELLPC on 2022/11/22.
 //
-#include "bits/stdc++.h"
-using namespace std;
-typedef struct number{
-    int n;
-    int grade;
-    number(int n,int grade):n(n),grade(grade){}
-};
-bool cmp1(number n1,number n2){
-    return n1.grade<n2.grade;
-};
-bool cmp2(number n1,number n2){
-    return n1.n<n2.n;
-}
+#include<iostream>
+#include<cstdio>
 
 int main(){
-    int n,x;
-
-    vector<number >v,v1,v2;
-    cin>>n;
-    for (int i = 0; i < n; ++i) {
-        cin>>x;
-
-        v.push_back(number(i,x));
-
-    }
-
-    sort(v.begin(),v.end(), cmp1);
-    int grade;
-    cin>>grade;
-    int r=v.size()-1;
-    if(v[r].grade<grade){
-        sort(v.begin(),v.end(), cmp2);
-        for (auto j = v.begin(); j!=v.end() ; ++j) {
-            cout<<(*j).grade<<" ";
-        }
-        cout<<endl;
-        return 0;
-    }
-
-    int i;
-    if(v[0].grade<grade) {
-        v1.push_back(v[0]);
-        for (i = 1; i < n; ++i) {
-            v1.push_back(v[i]);
-            if (v[i].grade >= grade && v[i - 1].grade < grade) {
-                v1.pop_back();
-                break;
-            }
-        }
-    }
-    else i=0;
-
-    for ( i ; i <n ; ++i) {
-       v2.push_back(v[i]);
-    }
-    sort(v1.begin(),v1.end(), cmp2);
-
-    sort(v2.begin(),v2.end(), cmp2);
-    for (auto j = v1.begin(); j!=v1.end() ; ++j) {
-        cout<<(*j).grade<<" ";
-    }
-    for (auto j = v2.begin(); j!=v2.end() ; ++j) {
-        cout<<(*j).grade<<" ";
-    }
-    cout<<endl;
-    sort(v2.begin(),v2.end(), cmp1);
-    for (auto j = v2.begin(); j!=v2.end() ; ++j) {
-        cout<<(*j).grade<<" ";
-    }
 
 }
+//using namespace std;
+//int g[105],f[105],a[105],s[105];
+//int main()
+//{
+//    int n;
+//    cin>>n;
+//    for(int i=1;i<=n;i++)
+//    {
+//        cin>>a[i];
+//        f[i]=1;
+//        g[i]=1;
+//    }//输入并赋初值
+//    for(int i=n-1;i>=1;i--)
+//    {
+//        for(int j=i+1;j<=n;j++)
+//        {
+//            if(a[i]>a[j]&&f[i]<=f[j]+1)
+//            {
+//                f[i]=f[j]+1;
+//            }
+//        }
+//    }//从右往左，按左高右低顺序找出每一个位置右边有几个从高到低的数 （包括自己）
+//    for(int i=2;i<=n;i++)
+//    {
+//        for(int j=1;j<i;j++)
+//        {
+//            if(a[i]>a[j]&&g[i]<=g[j]+1)
+//            {
+//                g[i]=g[j]+1;
+//            }
+//        }
+//    }//从左往右，按左低右高顺序找出每一个位置左边有几个从低到高的数 （包括自己）
+//    int maxx=0;
+//    for(int i=1;i<=n;i++)
+//    {
+//        s[i]=f[i]+g[i]-1;//把每个数的左边从低到高的数和右边从高到低的数相加
+//        //注意！！自己加了两次要-1
+//        if(s[i]>maxx)
+//        {
+//            maxx=s[i];
+//        }
+//    }
+//    cout<<n-maxx;//是求出列的人数
+//}
+//#include "bits/stdc++.h"
+//using namespace std;
+//int main(){
+//    priority_queue<int> q;
+//    int n,s;
+//    cin>>n;
+//    for (int i = 0; i < n; ++i) {
+//        cin>>s;
+//        q.push(s);
+//    }
+//    cout<<n;
+//    for (int i = 0; i < ; ++i) {
+//
+//    }
+//}
+//#include "bits/stdc++.h"
+//using namespace std;
+//bool isp[10000004];
+//int sum=0;
+////len 遍历的下标 n抽的个数 m总数 number 目前的个数 s目前的总和
+//void dfs(vector<int> v,int len,int n,int m,int number,int s){
+//
+//    if(n==number){
+//        if(isp[s]){
+//            sum+=1;
+//        }
+//        return;
+//    }
+//    if(len==m-1)return;
+//    for (int i = len+1; i < m; ++i) {
+//        dfs(v,i,n,m,number+1,s+v[i]);
+//    }
+//    return;
+//}
+//int main(){
+//    memset(isp, true,sizeof(isp));
+//    for (int i = 2; i < 10000000; ++i) {
+//        if(isp[i]){
+//            for (int j = 2; j <= 10000000/i; ++j) {
+//                isp[i*j]= false;
+//            }
+//        }
+//    }
+//    int m,n,s;
+//    vector<int>v;
+//    cin>>m>>n;
+//    for (int i = 0; i < m; ++i) {
+//        cin>>s;
+//        v.push_back(s);
+//    }
+//    dfs(v,-1,n,m,0,0);
+//    cout<<sum;
+//}
+//#include<cstdio>
+//double a,b,c,d;
+//double fc(double x)
+//{
+//    return a*x*x*x+b*x*x+c*x+d;
+//}
+//int main()
+//{
+//    double l,r,m,x1,x2;
+//    int s=0,i;
+//    scanf("%lf%lf%lf%lf",&a,&b,&c,&d);  //输入
+//    for (i=-100;i<100;i++)
+//    {
+//        l=i;
+//        r=i+1;
+//        x1=fc(l);
+//        x2=fc(r);
+//        if(!x1)
+//        {
+//            printf("%.2lf ",l);
+//            s++;
+//        }      //判断左端点，是零点直接输出。
+//
+//        //不能判断右端点，会重复。
+//        if(x1*x2<0)                             //区间内有根。
+//        {
+//            while(r-l>=0.001)                     //二分控制精度。
+//            {
+//                m=(l+r)/2;  //middle
+//                if(fc(m)*fc(r)<=0)
+//                    l=m;
+//                else
+//                    r=m;   //计算中点处函数值缩小区间。
+//            }
+//            printf("%.2lf ",r);
+//            //输出右端点。
+//            s++;
+//        }
+//        if (s==3)
+//            break;
+//        //找到三个就退出大概会省一点时间
+//    }
+//    return 0;
+//}
+
+//#include<iostream>
+//#include<fstream>
+//#include<cstdio>
+//#include<algorithm>
+//#include<cmath>
+//#include<string>
+//using namespace std;
+//int pd[10002][202],a[10002][202];
+//int main()
+//{
+//    //freopen("treasure.in","r",stdin);
+//    //freopen("treasure.out","w",stdout);
+//    int n,m,x,i,j,ans=0,l=0;
+//    scanf("%d%d",&n,&m);//以下的读入不说了
+//    for(i=1;i<=n;i++)
+//    {
+//        l=0;
+//        for(j=0;j<m;j++)
+//        {
+//            scanf("%d%d",&pd[i][j],&a[i][j]);
+//            if(pd[i][j]==1) l++;//记录该层楼有楼梯的门数
+//        }
+//        pd[i][m]=l;
+//    }
+//    scanf("%d",&x);
+//    i=1;
+//    while(i<=n)
+//    {
+//        ans+=a[i][x];
+//        ans%=20123;//根据规律，边加边模，值不变，不用long long
+//        int k=0;
+//        for(j=x;;j++)
+//        {
+//            if(j==m) j=0;
+//            if(pd[i][j]==1) k++;
+//            if(k==(a[i][x]-1)%pd[i][m]+1) break;//最关键的停止条件，循环找对，取模，都靠它。
+//        }
+//        x=j;
+//        i++;
+//    }
+//    printf("%d",ans);//小小的输出优化~
+//    return 0;
+//}
+//#include "bits/stdc++.h"
+//using namespace std;
+//typedef struct number{
+//    int n;
+//    int grade;
+//    number(int n,int grade):n(n),grade(grade){}
+//};
+//bool cmp1(number n1,number n2){
+//    return n1.grade<n2.grade;
+//};
+//bool cmp2(number n1,number n2){
+//    return n1.n<n2.n;
+//}
+//
+//int main(){
+//    int n,x;
+//
+//    vector<number >v,v1,v2;
+//    cin>>n;
+//    for (int i = 0; i < n; ++i) {
+//        cin>>x;
+//
+//        v.push_back(number(i,x));
+//
+//    }
+//
+//    sort(v.begin(),v.end(), cmp1);
+//    int grade;
+//    cin>>grade;
+//    int r=v.size()-1;
+//    if(v[r].grade<grade){
+//        sort(v.begin(),v.end(), cmp2);
+//        for (auto j = v.begin(); j!=v.end() ; ++j) {
+//            cout<<(*j).grade<<" ";
+//        }
+//        cout<<endl;
+//        return 0;
+//    }
+//
+//    int i;
+//    if(v[0].grade<grade) {
+//        v1.push_back(v[0]);
+//        for (i = 1; i < n; ++i) {
+//            v1.push_back(v[i]);
+//            if (v[i].grade >= grade && v[i - 1].grade < grade) {
+//                v1.pop_back();
+//                break;
+//            }
+//        }
+//    }
+//    else i=0;
+//
+//    for ( i ; i <n ; ++i) {
+//       v2.push_back(v[i]);
+//    }
+//    sort(v1.begin(),v1.end(), cmp2);
+//
+//    sort(v2.begin(),v2.end(), cmp2);
+//    for (auto j = v1.begin(); j!=v1.end() ; ++j) {
+//        cout<<(*j).grade<<" ";
+//    }
+//    for (auto j = v2.begin(); j!=v2.end() ; ++j) {
+//        cout<<(*j).grade<<" ";
+//    }
+//    cout<<endl;
+//    sort(v2.begin(),v2.end(), cmp1);
+//    for (auto j = v2.begin(); j!=v2.end() ; ++j) {
+//        cout<<(*j).grade<<" ";
+//    }
+//
+//}
 //#include <iostream>
 //#define maxn 55
 //using namespace std;
